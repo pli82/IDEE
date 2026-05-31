d9ab48745c95161bbe36153db98da8199bcb4417
pli82 <loredanairinapop82@gmail.com>
2026-05-31 11:47:56 +0300

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,3 @@
-// jest.config.ts
 import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
@@ -7,7 +6,6 @@ const createJestConfig = nextJest({ dir: './' })
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'node',
-  setupFilesAfterFramework: ['<rootDir>/tests/setup.ts'],
   setupFilesAfterFramework: ['<rootDir>/tests/setup.ts'],
   testMatch: [
     '<rootDir>/tests/unit/**/*.test.ts',
