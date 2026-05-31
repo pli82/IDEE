1dc8185fd83c3902c6e26e91b7aec09f8d313325
pli82 <loredanairinapop82@gmail.com>
2026-05-31 12:29:22 +0300

    Update page.tsx

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic'
 'use client'
+export const dynamic = 'force-dynamic'
 import { useState } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
 import Link from 'next/link'
