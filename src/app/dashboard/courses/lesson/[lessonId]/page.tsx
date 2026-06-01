8108a5f54fa693bd87f8e87f507e256e1aab40cc
pli82 <loredanairinapop82@gmail.com>
2026-06-01 10:27:53 +0300

    Update page.tsx

--- src/app/dashboard/courses/lesson/[lessonId]/page.tsx
+++ src/app/dashboard/courses/lesson/[lessonId]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
 
